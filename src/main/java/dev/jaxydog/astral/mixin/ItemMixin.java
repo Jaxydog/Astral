fdf4a0b9dade77eb1a1301b395d05e820d267336
jaxydog <32988901+jaxydog@users.noreply.github.com>
2023-05-23 07:27:13 -0400

    Add `SetGlint` NBT tag

---
+++ src/main/java/dev/jaxydog/astral/mixin/ItemMixin.java
@@ -0,0 +1,23 @@
+package dev.jaxydog.astral.mixin;
+
+import dev.jaxydog.astral.utility.NbtHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/** Provide an NBT tag to disable enchantment glint */
+@Mixin(Item.class)
+public abstract class ItemMixin {
+
+	private static final String SET_GLINT_KEY = "SetGlint";
+
+	@Inject(method = "hasGlint", at = @At("HEAD"), cancellable = true)
+	private void hasGlintInject(ItemStack stack, CallbackInfoReturnable<Boolean> callbackInfo) {
+		if (NbtHelper.contains(stack, ItemMixin.SET_GLINT_KEY)) {
+			callbackInfo.setReturnValue(NbtHelper.getBoolean(stack, ItemMixin.SET_GLINT_KEY));
+		}
+	}
+}
