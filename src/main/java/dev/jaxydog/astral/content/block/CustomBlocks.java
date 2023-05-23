02bfe14c3dd859da96fd6567ee89bfe6a19bb530
jaxydog <32988901+jaxydog@users.noreply.github.com>
2023-05-23 10:51:18 -0400

    Add custom blocks and items

--- src/main/java/dev/jaxydog/astral/content/block/CustomBlocks.java
+++ src/main/java/dev/jaxydog/astral/content/block/CustomBlocks.java
@@ -1,7 +1,31 @@
 package dev.jaxydog.astral.content.block;
 
+import dev.jaxydog.astral.content.block.custom.DyedAmethystBlock;
+import dev.jaxydog.astral.content.block.custom.DyedAmethystClusterBlock;
 import dev.jaxydog.astral.utility.AutoRegister;
+import dev.jaxydog.astral.utility.DyeableHelper;
+import net.minecraft.block.AbstractBlock.Settings;
+import net.minecraft.block.Material;
+import net.minecraft.sound.BlockSoundGroup;
 
 /** Contains definitions for all custom blocks */
 @AutoRegister
-public class CustomBlocks {}
+public class CustomBlocks {
+
+	public static final DyeableHelper<DyedAmethystBlock> DYED_AMETHYST_BLOCK = new DyeableHelper<>(
+		"amethyst_block",
+		Settings.of(Material.AMETHYST).sounds(BlockSoundGroup.AMETHYST_BLOCK).strength(1.5F).requiresTool(),
+		DyedAmethystBlock::new
+	);
+	public static final DyeableHelper<DyedAmethystClusterBlock> DYED_AMETHYST_CLUSTER = new DyeableHelper<>(
+		"amethyst_cluster",
+		Settings
+			.of(Material.AMETHYST)
+			.luminance(state -> 5)
+			.nonOpaque()
+			.sounds(BlockSoundGroup.AMETHYST_CLUSTER)
+			.strength(1.5F)
+			.requiresTool(),
+		DyedAmethystClusterBlock::new
+	);
+}
--- src/main/java/dev/jaxydog/astral/content/item/CustomItems.java
+++ src/main/java/dev/jaxydog/astral/content/item/CustomItems.java
@@ -1,16 +1,56 @@
 package dev.jaxydog.astral.content.item;
 
+import dev.jaxydog.astral.content.block.CustomBlocks;
+import dev.jaxydog.astral.content.item.CustomArmorItem.SetHelper;
+import dev.jaxydog.astral.content.item.custom.CloudyArmorItem;
+import dev.jaxydog.astral.content.item.custom.CloudyItem;
 import dev.jaxydog.astral.content.item.custom.CurrencyItem;
 import dev.jaxydog.astral.content.item.custom.CurrencyRewardItem;
 import dev.jaxydog.astral.content.item.custom.CurrencySkeletonItem;
 import dev.jaxydog.astral.utility.AutoRegister;
+import dev.jaxydog.astral.utility.DyeableHelper;
 import java.util.HashMap;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item.Settings;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Rarity;
 
 /** Contains definitions for all custom items */
 @AutoRegister
 public class CustomItems {
+
+	public static final CloudyItem CLOUDY_CANDY = new CloudyItem(
+		"cloudy_candy",
+		new Settings()
+			.food(
+				new FoodComponent.Builder()
+					.alwaysEdible()
+					.hunger(2)
+					.saturationModifier(0.45f)
+					.snack()
+					.statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 100, 0, false, true, true), 0.25F)
+					.build()
+			)
+			.rarity(Rarity.UNCOMMON)
+	);
+	public static final CloudyItem CLOUDY_COTTON = new CloudyItem(
+		"cloudy_cotton",
+		new Settings().rarity(Rarity.UNCOMMON)
+	);
+	public static final CloudyItem CLOUDY_MANE = new CloudyItem("cloudy_mane", new Settings().rarity(Rarity.UNCOMMON));
+	public static final SetHelper<CloudyArmorItem> CLOUDY_ARMOR = new SetHelper<>(
+		new CustomArmorMaterial("cloudy")
+			.setDurability(4.0F)
+			.setEnchantability(15)
+			.setEquipSound(SoundEvents.ITEM_ARMOR_EQUIP_LEATHER)
+			.setProtectionAmount(2, 5, 3, 2)
+			.setRepairIngredient(Ingredient.ofItems(CustomItems.CLOUDY_COTTON)),
+		new Settings().rarity(Rarity.UNCOMMON),
+		CloudyArmorItem::new
+	);
 
 	public static final CurrencyItem CURRENCY = new CurrencyItem("currency", new Settings().rarity(Rarity.UNCOMMON));
 	public static final CurrencyRewardItem CURRENCY_REWARD = new CurrencyRewardItem(
@@ -21,5 +61,18 @@ public class CustomItems {
 	public static final CurrencySkeletonItem CURRENCY_SKELETON = new CurrencySkeletonItem(
 		"currency_skeleton",
 		new Settings().maxCount(1).rarity(Rarity.EPIC)
+	);
+
+	public static final DyeableHelper<CustomBlockItem> DYED_AMETHYST_BLOCK = DyeableHelper.fromDyeable(
+		"amethyst_block",
+		CustomBlocks.DYED_AMETHYST_BLOCK,
+		new Settings(),
+		CustomBlockItem::new
+	);
+	public static final DyeableHelper<CustomBlockItem> DYED_AMETHYST_CLUSTER = DyeableHelper.fromDyeable(
+		"amethyst_cluster",
+		CustomBlocks.DYED_AMETHYST_CLUSTER,
+		new Settings(),
+		CustomBlockItem::new
 	);
 }
