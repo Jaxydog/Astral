d4c9ff5b9128f45aa884f3816607a0ab0c754714
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-07-19 15:41:11 -0400

    Update custom command class

--- src/main/java/dev/jaxydog/content/command/CustomCommand.java
+++ src/main/java/dev/jaxydog/content/command/CustomCommand.java
@@ -2,6 +2,7 @@ package dev.jaxydog.content.command;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.jaxydog.utility.register.Registerable;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.server.command.CommandManager;
@@ -18,15 +19,15 @@ public abstract class CustomCommand implements Registerable.Main {
 	}
 
 	/** Executes the command */
-	public abstract int execute(CommandContext<ServerCommandSource> context);
+	public abstract int execute(CommandContext<ServerCommandSource> context)
+			throws CommandSyntaxException;
 
 	/** A predicate that determines if a source can use the command */
 	public abstract boolean requires(ServerCommandSource source);
 
 	/** Returns the command's builder */
 	public LiteralArgumentBuilder<ServerCommandSource> getCommand() {
-		return CommandManager.literal(this.getRawId()).requires(this::requires)
-				.executes(this::execute);
+		return CommandManager.literal(this.getRawId()).requires(this::requires);
 	}
 
 	@Override
@@ -36,8 +37,8 @@ public abstract class CustomCommand implements Registerable.Main {
 
 	@Override
 	public void registerMain() {
-		CommandRegistrationCallback.EVENT.register(
-				(dispatcher, registry, environment) -> dispatcher.register(this.getCommand()));
+		CommandRegistrationCallback.EVENT.register((dispatcher, registry, environment) -> dispatcher
+				.register(this.getCommand().executes(this::execute)));
 	}
 
 }
