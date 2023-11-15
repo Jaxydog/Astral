b9699c501440fdbb5074f6d91f9937454899e045
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-15 18:20:56 -0500

    Change challenge additive gamerules to be doubles

--- src/main/java/dev/jaxydog/content/CustomGamerules.java
+++ src/main/java/dev/jaxydog/content/CustomGamerules.java
@@ -23,12 +23,12 @@ public final class CustomGamerules extends ContentContainer {
 			.register("challengeChunkStep", Category.MOBS, GameRuleFactory.createIntRule(16));
 	/** Defines the gamerule that configures how many attack points are added every chunk step */
 	@Skip
-	public static final Key<IntRule> CHALLENGE_ATTACK_ADDITIVE = GameRuleRegistry
-			.register("challengeAttackAdditive", Category.MOBS, GameRuleFactory.createIntRule(1));
+	public static final Key<DoubleRule> CHALLENGE_ATTACK_ADDITIVE = GameRuleRegistry.register(
+			"challengeAttackAdditive", Category.MOBS, GameRuleFactory.createDoubleRule(1.0D));
 	/** Defines the gamerule that configures how many health points are added every chunk step */
 	@Skip
-	public static final Key<IntRule> CHALLENGE_HEALTH_ADDITIVE = GameRuleRegistry
-			.register("challengeHealthAdditive", Category.MOBS, GameRuleFactory.createIntRule(1));
+	public static final Key<DoubleRule> CHALLENGE_HEALTH_ADDITIVE = GameRuleRegistry.register(
+			"challengeHealthAdditive", Category.MOBS, GameRuleFactory.createDoubleRule(1.0D));
 
 	/** Defines the gamerule that configures the chance of crafting a reward from currency */
 	@Skip
