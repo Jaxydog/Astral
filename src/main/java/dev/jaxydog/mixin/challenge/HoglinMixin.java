97d59bfa0c1095277d9bd2fda288ecbe1ca41839
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 05:20:23 -0500

    Scale hoglins and zoglins

---
+++ src/main/java/dev/jaxydog/mixin/challenge/HoglinMixin.java
@@ -0,0 +1,29 @@
+package dev.jaxydog.mixin.challenge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import dev.jaxydog.utility.MobChallengeUtil;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.mob.Hoglin;
+
+@Mixin(Hoglin.class)
+public interface HoglinMixin {
+
+	@ModifyArgs(method = "tryAttack", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/LivingEntity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+	private static void tryAttackArgsInject(Args args) {
+		final DamageSource source = args.get(0);
+
+		if (!MobChallengeUtil.shouldScale(source.getAttacker())) {
+			return;
+		}
+
+		final double additive = MobChallengeUtil.getAttackAdditive(source.getAttacker().getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(source.getAttacker(), additive);
+
+		args.set(1, args.<Float>get(1) + (float) scaled);
+	}
+
+}
