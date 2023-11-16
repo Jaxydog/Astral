6708ebd3439f54bda9a50c7bab56e59f34d59026
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 10:40:13 -0500

    Scale shulkers

---
+++ src/main/java/dev/jaxydog/mixin/challenge/ShulkerBulletEntityMixin.java
@@ -0,0 +1,34 @@
+package dev.jaxydog.mixin.challenge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import dev.jaxydog.utility.MobChallengeUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.entity.projectile.ShulkerBulletEntity;
+import net.minecraft.world.World;
+
+@Mixin(ShulkerBulletEntity.class)
+public abstract class ShulkerBulletEntityMixin extends ProjectileEntity {
+
+	public ShulkerBulletEntityMixin(EntityType<? extends ProjectileEntity> entityType,
+			World world) {
+		super(entityType, world);
+	}
+
+	@ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float onEntityHitArgsInject(float damage) {
+		if (this.getOwner() != null && !MobChallengeUtil.shouldScale(this.getOwner())) {
+			return damage;
+		}
+
+		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(this.getOwner(), additive);
+
+		return damage + (float) scaled;
+	}
+
+}
