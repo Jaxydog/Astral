b6d69d6c1dd8ed6a5002f0d3aadd8b49ccde7a58
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 10:45:11 -0500

    Scale ender dragons

---
+++ src/main/java/dev/jaxydog/mixin/challenge/EnderDragonEntityMixin.java
@@ -0,0 +1,48 @@
+package dev.jaxydog.mixin.challenge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import dev.jaxydog.utility.MobChallengeUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.world.World;
+
+@Mixin(EnderDragonEntity.class)
+public abstract class EnderDragonEntityMixin extends MobEntity implements Monster {
+
+	protected EnderDragonEntityMixin(EntityType<? extends MobEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@ModifyArg(method = "launchLivingEntities", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float launchLivingEntitiesArgsInject(float damage) {
+		if (!MobChallengeUtil.shouldScale(this)) {
+			return damage;
+		}
+
+		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
+
+		return damage + (float) scaled;
+	}
+
+	@ModifyArg(method = "damageLivingEntities", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float damageLivingEntitiesArgsInject(float damage) {
+		if (!MobChallengeUtil.shouldScale(this)) {
+			return damage;
+		}
+
+		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
+
+		return damage + (float) scaled;
+	}
+
+}
