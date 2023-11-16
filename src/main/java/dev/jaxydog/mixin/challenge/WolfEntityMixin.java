012f46cd995078f99c874086928490c8bc0e1ff0
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 05:02:35 -0500

    Simplify `WolfEntityMixin`

--- src/main/java/dev/jaxydog/mixin/challenge/WolfEntityMixin.java
+++ src/main/java/dev/jaxydog/mixin/challenge/WolfEntityMixin.java
@@ -2,13 +2,9 @@ package dev.jaxydog.mixin.challenge;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import dev.jaxydog.utility.MobChallengeUtil;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.passive.WolfEntity;
@@ -22,24 +18,18 @@ public abstract class WolfEntityMixin extends TameableEntity implements Angerabl
 		super(entityType, world);
 	}
 
-	@Inject(method = "tryAttack", at = @At("HEAD"), cancellable = true)
-	private void tryAttackInject(Entity target, CallbackInfoReturnable<Boolean> callbackInfo) {
-		if (!MobChallengeUtil.shouldScale(this) || this.isTamed()) {
-			return;
+	@ModifyArg(method = "tryAttack", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float tryAttackArgsInject(float damage) {
+		if (!MobChallengeUtil.shouldScale(this)) {
+			return damage;
 		}
 
-		final double base = this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
 		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
 		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
 
-		final DamageSource source = this.getDamageSources().mobAttack(this);
-		final boolean applyEffects = target.damage(source, (float) (base + scaled));
-
-		if (applyEffects) {
-			this.applyDamageEffects(this, target);
-		}
-
-		callbackInfo.setReturnValue(applyEffects);
+		return damage + (float) scaled;
 	}
 
 }
