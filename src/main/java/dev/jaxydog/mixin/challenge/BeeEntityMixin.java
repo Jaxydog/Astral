3d84a3d2db91646227c2d1122f6c80c1c076ade6
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 04:55:33 -0500

    Make `BeeEntityMixin` less impactful

--- src/main/java/dev/jaxydog/mixin/challenge/BeeEntityMixin.java
+++ src/main/java/dev/jaxydog/mixin/challenge/BeeEntityMixin.java
@@ -3,22 +3,13 @@ package dev.jaxydog.mixin.challenge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import dev.jaxydog.utility.MobChallengeUtil;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.Flutterer;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.BeeEntity;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 
 @Mixin(BeeEntity.class)
@@ -31,47 +22,18 @@ public abstract class BeeEntityMixin extends AnimalEntity implements Angerable, 
 	@Invoker("setHasStung")
 	public abstract void setHasStungInvoker(boolean hasStung);
 
-	@Inject(method = "tryAttack", at = @At("HEAD"), cancellable = true)
-	private void tryAttackInject(Entity target, CallbackInfoReturnable<Boolean> callbackInfo) {
+	@ModifyArg(method = "tryAttack", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float tryAttackArgsInject(float damage) {
 		if (!MobChallengeUtil.shouldScale(this)) {
-			return;
+			return damage;
 		}
 
-		final double base = this.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
 		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
 		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
 
-		final DamageSource source = this.getDamageSources().sting(this);
-		final boolean applyEffects = target.damage(source, (float) (base + scaled));
-
-		if (applyEffects) {
-			this.applyDamageEffects(this, target);
-
-			if (target instanceof LivingEntity living) {
-				living.setStingerCount(living.getStingerCount() + 1);
-
-				int duration = 0;
-
-				if (this.getWorld().getDifficulty() == Difficulty.NORMAL) {
-					duration = 10;
-				} else if (this.getWorld().getDifficulty() == Difficulty.HARD) {
-					duration = 18;
-				}
-
-				if (duration > 0) {
-					final StatusEffectInstance status =
-							new StatusEffectInstance(StatusEffects.POISON, duration * 20, 0);
-
-					living.addStatusEffect(status, this);
-				}
-			}
-
-			this.setHasStungInvoker(true);
-			this.stopAnger();
-			this.playSound(SoundEvents.ENTITY_BEE_STING, 1.0f, 1.0f);
-		}
-
-		callbackInfo.setReturnValue(applyEffects);
+		return damage + (float) scaled;
 	}
 
 }
