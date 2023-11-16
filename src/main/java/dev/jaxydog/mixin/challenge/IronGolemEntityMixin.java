f150ece0df1fb6db62ae2ee92b24c90af2bb2b7b
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-11-16 04:44:25 -0500

    Scale iron golems

---
+++ src/main/java/dev/jaxydog/mixin/challenge/IronGolemEntityMixin.java
@@ -0,0 +1,34 @@
+package dev.jaxydog.mixin.challenge;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import dev.jaxydog.utility.MobChallengeUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.Angerable;
+import net.minecraft.entity.passive.GolemEntity;
+import net.minecraft.entity.passive.IronGolemEntity;
+import net.minecraft.world.World;
+
+@Mixin(IronGolemEntity.class)
+public abstract class IronGolemEntityMixin extends GolemEntity implements Angerable {
+
+	protected IronGolemEntityMixin(EntityType<? extends GolemEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@ModifyArg(method = "tryAttack", at = @At(value = "INVOKE",
+			target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"),
+			index = 1)
+	private float tryAttackArgsInject(float damage) {
+		if (!MobChallengeUtil.shouldScale(this)) {
+			return damage;
+		}
+
+		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
+
+		return damage + (float) scaled;
+	}
+
+}
