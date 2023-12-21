6c23838df869d2958476291eee59438cc9df02e1
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2023-12-21 17:01:20 -0500

    Use a tag to determine seed items Bump version

--- src/main/java/dev/jaxydog/mixin/ParrotEntityMixin.java
+++ src/main/java/dev/jaxydog/mixin/ParrotEntityMixin.java
@@ -1,12 +1,13 @@
 package dev.jaxydog.mixin;
 
-import com.google.common.collect.Sets;
+import dev.jaxydog.Astral;
 import net.minecraft.entity.EntityStatuses;
 import net.minecraft.entity.passive.ParrotEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -17,21 +18,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Set;
-
 /** Allows you to heal parrots with items */
 @Mixin(ParrotEntity.class)
 public abstract class ParrotEntityMixin {
 
 	@Unique
-	private static final Set<Item> SEEDS = Sets.newHashSet(
-		Items.WHEAT_SEEDS,
-		Items.MELON_SEEDS,
-		Items.PUMPKIN_SEEDS,
-		Items.BEETROOT_SEEDS,
-		Items.TORCHFLOWER_SEEDS,
-		Items.PITCHER_POD
-	);
+	private static final TagKey<Item> PARROT_FEED = TagKey.of(RegistryKeys.ITEM, Astral.getId("parrot_feed"));
 
 	@Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
 	private void interactMobInject(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> callbackInfo) {
@@ -39,7 +31,7 @@ public abstract class ParrotEntityMixin {
 		final ParrotEntity self = (ParrotEntity) (Object) this;
 		final World world = player.getWorld();
 		final boolean missingHealth = self.getHealth() < self.getMaxHealth();
-		final boolean canFeed = SEEDS.contains(stack.getItem());
+		final boolean canFeed = stack.isIn(PARROT_FEED);
 
 		if (world.isClient || !self.isTamed() || !canFeed || !missingHealth) return;
 
