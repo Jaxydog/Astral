e230d8c35987dab423bde8027c05f8957ac2ce6e
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-01-19 11:52:55 -0500

    Add Dolphin scaling

---
+++ src/main/java/dev/jaxydog/mixin/challenge/DolphinEntityMixin.java
@@ -0,0 +1,33 @@
+package dev.jaxydog.mixin.challenge;
+
+import dev.jaxydog.utility.MobChallengeUtil;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.WaterCreatureEntity;
+import net.minecraft.entity.passive.DolphinEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(DolphinEntity.class)
+public class DolphinEntityMixin extends WaterCreatureEntity {
+
+	protected DolphinEntityMixin(EntityType<? extends WaterCreatureEntity> entityType, World world) {
+		super(entityType, world);
+	}
+
+	@ModifyArg(
+		method = "tryAttack", at = @At(
+		value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"
+	), index = 1
+	)
+	private float tryAttackInject(float damage) {
+		if (!MobChallengeUtil.shouldScale(this)) return damage;
+
+		final double additive = MobChallengeUtil.getAttackAdditive(this.getWorld());
+		final double scaled = MobChallengeUtil.getScaledAdditive(this, additive);
+
+		return damage + (float) scaled;
+	}
+
+}
