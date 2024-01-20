b1af9985374e34c770f4ec4eb6bc43c65282154b
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-01-20 03:24:19 -0500

    Add sinister potion

---
+++ src/main/java/dev/jaxydog/content/effect/CustomPotions.java
@@ -0,0 +1,32 @@
+package dev.jaxydog.content.effect;
+
+import dev.jaxydog.content.effect.CustomPotion.Recipe;
+import dev.jaxydog.content.item.CustomItems;
+import dev.jaxydog.register.ContentRegistrar;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.util.DyeColor;
+
+public final class CustomPotions extends ContentRegistrar {
+
+	public static final CustomPotion SINISTER = new CustomPotion(
+		"sinister_potion",
+		new Recipe(Potions.THICK, Ingredient.ofItems(CustomItems.DYED_AMETHYST_CLUSTERS.get(DyeColor.RED))),
+		new StatusEffectInstance(CustomStatusEffects.SINISTER, 15 * 20, 0)
+	);
+
+	public static final CustomPotion LONG_SINISTER = new CustomPotion(
+		"long_sinister_potion",
+		new Recipe(CustomPotions.SINISTER, Ingredient.ofItems(Items.REDSTONE)),
+		new StatusEffectInstance(CustomStatusEffects.SINISTER, 30 * 20, 0)
+	);
+
+	public static final CustomPotion STRONG_SINISTER = new CustomPotion(
+		"strong_sinister_potion",
+		new Recipe(CustomPotions.SINISTER, Ingredient.ofItems(Items.GLOWSTONE_DUST)),
+		new StatusEffectInstance(CustomStatusEffects.SINISTER, 10 * 20, 1)
+	);
+
+}
