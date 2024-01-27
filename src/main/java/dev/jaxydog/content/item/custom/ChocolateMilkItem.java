86b381052069edc92433e219db4084c84499f1a7
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-01-27 03:05:50 -0500

    Group shared bottle item logic into base class

--- src/main/java/dev/jaxydog/content/item/custom/ChocolateMilkItem.java
+++ src/main/java/dev/jaxydog/content/item/custom/ChocolateMilkItem.java
@@ -1,36 +1,23 @@
 package dev.jaxydog.content.item.custom;
 
-import dev.jaxydog.content.item.CustomItem;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.item.SuspiciousStewItem;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
-public class ChocolateMilkItem extends CustomItem {
+public class ChocolateMilkItem extends BottleItem {
 
 	public ChocolateMilkItem(String rawId, Settings settings) {
 		super(rawId, settings);
 	}
 
 	@Override
-	public UseAction getUseAction(ItemStack stack) {
-		return UseAction.DRINK;
-	}
-
-	@Override
 	public ItemStack finishUsing(ItemStack stack, World world, LivingEntity entity) {
-		if (entity instanceof final PlayerEntity player && !player.isCreative()) {
-			player.giveItemStack(Items.GLASS_BOTTLE.getDefaultStack());
+		if (entity != null && !world.isClient()) {
+			SuspiciousStewItem.forEachEffect(stack, entity::addStatusEffect);
 		}
 
-		final ItemStack usedStack = super.finishUsing(stack, world, entity);
-
-		if (entity != null) SuspiciousStewItem.forEachEffect(usedStack, entity::addStatusEffect);
-
-		return stack;
+		return super.finishUsing(stack, world, entity);
 	}
 
 }
