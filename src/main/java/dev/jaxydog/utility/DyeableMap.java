896e2eac609821c572d6f49d6164107639ca4ec5
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-02-11 22:38:03 -0500

    Sort by color

--- src/main/java/dev/jaxydog/utility/DyeableMap.java
+++ src/main/java/dev/jaxydog/utility/DyeableMap.java
@@ -3,28 +3,48 @@ package dev.jaxydog.utility;
 import dev.jaxydog.register.Registered;
 import net.minecraft.util.DyeColor;
 
+import java.util.List;
 import java.util.Set;
 import java.util.function.BiFunction;
 
 public class DyeableMap<T extends Registered> extends RegisteredMap<DyeColor, T> {
 
-	public DyeableMap(String rawId, BiFunction<String, DyeColor, T> constructor) {
-		super(rawId, constructor);
-	}
+    private static final List<DyeColor> ORDER = List.of(
+        DyeColor.WHITE,
+        DyeColor.LIGHT_GRAY,
+        DyeColor.GRAY,
+        DyeColor.BLACK,
+        DyeColor.BROWN,
+        DyeColor.RED,
+        DyeColor.ORANGE,
+        DyeColor.YELLOW,
+        DyeColor.LIME,
+        DyeColor.GREEN,
+        DyeColor.CYAN,
+        DyeColor.LIGHT_BLUE,
+        DyeColor.BLUE,
+        DyeColor.PURPLE,
+        DyeColor.MAGENTA,
+        DyeColor.PINK
+    );
 
-	@Override
-	public final Set<DyeColor> keys() {
-		return Set.of(DyeColor.values());
-	}
+    public DyeableMap(String rawId, BiFunction<String, DyeColor, T> constructor) {
+        super(rawId, constructor);
+    }
 
-	@Override
-	public final String getIdPath(DyeColor key) {
-		return String.format("%s_%s", key.asString(), this.getRegistryIdPath());
-	}
+    @Override
+    public final Set<DyeColor> keys() {
+        return Set.of(DyeColor.values());
+    }
 
-	@Override
-	protected final int compareKeys(DyeColor a, DyeColor b) {
-		return a.compareTo(b);
-	}
+    @Override
+    public final String getIdPath(DyeColor key) {
+        return String.format("%s_%s", key.asString(), this.getRegistryIdPath());
+    }
+
+    @Override
+    protected final int compareKeys(DyeColor a, DyeColor b) {
+        return Integer.compare(ORDER.indexOf(a), ORDER.indexOf(b));
+    }
 
 }
