c2ec14ea51faf240427de4dfdf90efa653dbea70
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-03-03 20:23:48 -0500

    Fix cow texture when ETF is installed

--- src/main/java/dev/jaxydog/mixin/client/CowEntityRendererMixin.java
+++ src/main/java/dev/jaxydog/mixin/client/CowEntityRendererMixin.java
@@ -1,7 +1,9 @@
 package dev.jaxydog.mixin.client;
 
+import com.google.common.base.Suppliers;
 import dev.jaxydog.Astral;
 import dev.jaxydog.utility.CowType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.render.entity.CowEntityRenderer;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.util.Identifier;
@@ -11,11 +13,23 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.function.Supplier;
+
 @Mixin(CowEntityRenderer.class)
 public class CowEntityRendererMixin {
 
     @Unique
-    private static final Identifier PINK_TEXTURE = Astral.getId("textures/entity/cow/pink_cow.png");
+    private static final Identifier PINK_TEXTURE_DEFAULT = Astral.getId("textures/entity/cow/pink_cow.png");
+    @Unique
+    private static final Identifier PINK_TEXTURE_ETF = Astral.getId("textures/entity/cow/pink_cow_etf.png");
+    @Unique
+    private static final Supplier<Identifier> PINK_TEXTURE = Suppliers.memoize(() -> {
+        if (FabricLoader.getInstance().getModContainer("entity_texture_features").isPresent()) {
+            return PINK_TEXTURE_ETF;
+        } else {
+            return PINK_TEXTURE_DEFAULT;
+        }
+    });
 
     @Inject(
         method = "getTexture(Lnet/minecraft/entity/passive/CowEntity;)Lnet/minecraft/util/Identifier;",
@@ -24,7 +38,7 @@ public class CowEntityRendererMixin {
     )
     private void maybePinkTexture(CowEntity cowEntity, CallbackInfoReturnable<Identifier> callbackInfo) {
         if (cowEntity.getDataTracker().get(CowType.COW_TYPE).equals(CowType.PINK.asString())) {
-            callbackInfo.setReturnValue(PINK_TEXTURE);
+            callbackInfo.setReturnValue(PINK_TEXTURE.get());
         }
     }
 
