562aa27fc769ad55c369914e2079dbc8e21e7d63
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-03-03 23:09:49 -0500

    Allow strawberry milk to provide food and saturation

--- src/main/java/dev/jaxydog/content/item/custom/BottleItem.java
+++ src/main/java/dev/jaxydog/content/item/custom/BottleItem.java
@@ -10,6 +10,7 @@ import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -50,24 +51,39 @@ public class BottleItem extends CustomItem {
     public ItemStack finishUsing(ItemStack stack, World world, LivingEntity entity) {
         final @Nullable PlayerEntity player = entity instanceof PlayerEntity ? (PlayerEntity) entity : null;
 
+        if (player != null && !player.isCreative()) stack.decrement(1);
+
+        entity.emitGameEvent(GameEvent.DRINK);
+
+        // This mimics the behavior of `PlayerEntity#eatFood` while allowing more customization.
+        if (stack.isFood()) {
+            if (player != null) {
+                final float pitchVariance = world.getRandom().nextFloat() * 0.1F;
+
+                player.getHungerManager().eat(stack.getItem(), stack);
+                player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+                player.playSound(SoundEvents.ENTITY_PLAYER_BURP, 0.5F, 0.9F + pitchVariance);
+
+                // Usually, `CONSUME_ITEM` is triggered here, but we already trigger it later.
+            }
+
+            // Don't play the eating sound that would be on this line, as this is not food.
+            entity.applyFoodEffects(stack, world, entity);
+
+            // Usually, decrementing the stack and emitting a game event is handled here.
+            // Instead, we handle it above this condition, as bottle items are always consumed.
+        }
+
         if (player instanceof final ServerPlayerEntity serverPlayer) {
             Criteria.CONSUME_ITEM.trigger(serverPlayer, stack);
         }
 
-        if (player != null) {
-            player.incrementStat(Stats.USED.getOrCreateStat(this));
-
-            if (!player.isCreative()) stack.decrement(1);
-        }
-
-        // Give the player a bottle when drinking, unless they're in creative.
+        // Give the entity a bottle when drinking, unless they're a player in creative mode.
         if (player == null || !player.isCreative()) {
             if (stack.isEmpty()) return Items.GLASS_BOTTLE.getDefaultStack();
 
             if (player != null) player.giveItemStack(Items.GLASS_BOTTLE.getDefaultStack());
         }
-
-        entity.emitGameEvent(GameEvent.DRINK);
 
         return stack;
     }
